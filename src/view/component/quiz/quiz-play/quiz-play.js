806963f257645eb8049b09ed8e1fdd84d972d854
kyoohyun <kgh2120@naver.com>
2022-11-04 16:18:07 +0900

    view : 퀴즈 추가 뷰 제작

---
+++ src/view/component/quiz/quiz-play/quiz-play.js
@@ -0,0 +1,12 @@
+import My_Navbar from "../../navbar/my_Navbar";
+export default function QuizPlay() {
+
+    //src는 mockup용으로 세워둠.
+    return<>
+    <My_Navbar></My_Navbar>
+    <div className="quiz_background">
+        <img className="quiz_img m-0 m-auto" src="https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202207/28/e4727123-666e-4603-a2fa-b2478b3130bd.jpg"
+             alt="..."/>
+    </div>
+    </>
+};
