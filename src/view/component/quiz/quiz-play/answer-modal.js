581911ec922e34b653cc317b6eb3951fc7ddb069
kyoohyun <kgh2120@naver.com>
2022-11-11 12:48:56 +0900

    FEAT : 퀴즈 플레이 후 메인 페이지 이동 기능 구현

--- src/view/component/quiz/quiz-play/answer-modal.js
+++ src/view/component/quiz/quiz-play/answer-modal.js
@@ -1,8 +1,14 @@
 import {Modal, Button} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
 
 export default function AnswerModal({_show, _setShow, _name}) {
 
+    const navigate = useNavigate();
+
     const answer_close = () => _setShow(false);
+    const moveToMain = () =>{
+        navigate("/", { replace: true});
+    }
 
     return <>
         <Modal className={"answer_box"} show={_show} onHide={answer_close} centered>
@@ -11,7 +17,7 @@ export default function AnswerModal({_show, _setShow, _name}) {
             </Modal.Header>
             <Modal.Body className={"answer_body"}>{_name}</Modal.Body>
             <Modal.Footer className={"answer_footer"}>
-                <Button className={"w-45"} variant="success">
+                <Button className={"w-45"} variant="success" onClick={moveToMain}>
                     돌아가기
                 </Button>
                 <Button className={"w-45"} variant="warning" >
