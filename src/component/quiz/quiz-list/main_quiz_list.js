b1063eee465780b36effd9ea447ee800a5f2febe
kyoohyun <kgh2120@naver.com>
2022-11-24 10:53:30 +0900

    REFACTOR : 퀴즈 검색 로직 리팩토링

--- src/component/quiz/quiz-list/main_quiz_list.js
+++ src/component/quiz/quiz-list/main_quiz_list.js
@@ -2,35 +2,55 @@ import QuizList from "./quiz_list";
 import QuizSearchBlock from "./quizSearchBlock";
 import My_Navbar from "../../navbar/my_Navbar";
 import {useEffect, useState} from "react";
-import {autoLogin, getAccessToken} from "../../../utils/global/token";
+import {autoLogin, getAccessToken, getRefreshToken} from "../../../utils/global/token";
+import {baseAxios} from "../../../utils/global/axios-config";
 
 
 
 export default function Main_quiz_list(){
 
     const [currentPageNum, setCurrentPageNum] = useState(0)
-    const [token, setToken] = useState();
-
+    const[searchCondition, setSearchCondition] = useState({
+        pageNum : 0,
+        answerName : "",
+        orderCondition : "POPULAR",
+        tagNames : []
+    });
     const [quiz, setQuiz] = useState({
         quizzes : [],
         nextPageNum : 0,
         hasNext : true
     })
     useEffect(() => {
-        if (!getAccessToken()) {
+        if (!getAccessToken() && getRefreshToken()) {
             autoLogin();
-
         }
-        console.log("app initialized")
     }, [])
 
+    const searchQuiz = () => {
+        console.log(searchCondition)
+        baseAxios.post("/quiz", searchCondition)
+            .then(response => {
+                console.log(response)
+                const result = {
+                    quizzes: response.data.quizzes,
+                    nextPageNum: response.data.nextPageNum,
+                    hasNext: response.data.hasNext,
+                }
+                setQuiz(result);
+            }).catch(err => {
+            alert("검색 결과가 없습니다.");
+        })
+    }
 
-
+    useEffect(() => {
+        searchQuiz()
+    },[searchCondition])
 
     return <>
         <My_Navbar></My_Navbar>
-        <QuizSearchBlock _setQuiz={setQuiz} _pageNum={currentPageNum} _setPageNum={setCurrentPageNum}></QuizSearchBlock>
-        <QuizList _quiz={quiz}></QuizList>
+        <QuizSearchBlock _setSearchCondition={setSearchCondition}></QuizSearchBlock>
+        <QuizList _quiz={quiz} _setQuiz={setQuiz}></QuizList>
 
     </>
 }
